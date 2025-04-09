8378cc168d05c14b7c284e3d1b6874e82f78e152
Czkafek <czkafek@gmail.com>
2025-04-09 21:27:01 +0200

    ye

--- client/src/scripts/api.js
+++ client/src/scripts/api.js
@@ -8,9 +8,11 @@ const api = axios.create({
 
 api.interceptors.request.use(async config => {
 /*
-1. Sprawdza czy masz access token, jeśli nie to cię odsyła na /refresh_token
-2. Sprawdza czy access token jest valid, jeśli nie to cię odsyła na /refresh_token
-3. Podczepia access token do configu i puszcza dalej
+1. Sprawdza czy masz access token w localstorage, jeśli nie to cię odsyła na /refresh_token
+2. Sprawdza czy access token jest valid po przez użycie jwtDecode i sprawdzenia daty w tokenie z obecną datą, jeśli nie to cię odsyła na /refresh_token
+3. Jeśli wynik z funkcji zajmującej się komunikacją z /refresh_token jest poprawny to tworzy string w postaci 'Bearer ' + accessToken i następnie podczepa do configu i puszcza dalej,
+   Jeśli jest niepoprawny nie tworzy stringu i nie dodaje go do configu
+4. Zwraca config
 */
 }, (error) => {
     return Promise.reject(error);

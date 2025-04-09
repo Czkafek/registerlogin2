23ccac041530decafaccd8459eab758bd4574594
Czkafek <czkafek@gmail.com>
2025-04-09 19:31:56 +0200

    ye

--- client/src/scripts/api.js
+++ client/src/scripts/api.js
@@ -7,7 +7,11 @@ const api = axios.create({
 
 
 api.interceptors.request.use(async config => {
-
+/*
+1. Sprawdza czy masz access token, jeśli nie to cię odsyła na /refresh_token
+2. Sprawdza czy access token jest valid, jeśli nie to cię odsyła na /refresh_token
+3. Podczepia access token do configu i puszcza dalej
+*/
 }, (error) => {
     return Promise.reject(error);
 })
